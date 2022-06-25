ac2cfd28c799acca12cf96a4e624b9db771086be
Dmitriy Vi <nyamaste@users.noreply.github.com>
2022-06-24 20:52:47 -0700

    rewriting terraform

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,40 +1,109 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "17.24.0"
+  version         = "~> 18.0"
+
   cluster_name    = local.cluster_name
   cluster_version = "1.22"
   subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
-  vpc_id = module.vpc.vpc_id
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
 
-  workers_group_defaults = {
-    root_volume_type = "gp2"
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
   }
 
-  worker_groups_launch_template = [
-    {
-      name                          = "worker-group-1"
-      instance_types                = ["t2.medium", "t3.medium"]
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      spot_allocation_strategy      = "lowest-price"
-      asg_max_size                  = 3
-      asg_desired_capacity          = 2
-      kubelet_extra_args            = "--node-labels=node.kubernetes.io/lifecycle=spot"
-    },
-  ]
+  cluster_encryption_config = [{
+    provider_key_arn = aws_kms_key.eks.arn
+    resources        = ["secrets"]
+  }]
 
-  worker_groups = [
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t3.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_max_size                  = 3
-      asg_desired_capacity          = 1
-      kubelet_extra_args            = "--node-labels=node.kubernetes.io/lifecycle=ondemand"
-    },
-  ]
+  # Self managed node groups will not automatically create the aws-auth configmap so we need to
+  create_aws_auth_configmap = true
+  manage_aws_auth_configmap = true
+
+  # Extend cluster security group rules
+  cluster_security_group_additional_rules = {
+    egress_nodes_ephemeral_ports_tcp = {
+      description                = "To node 1025-65535"
+      protocol                   = "tcp"
+      from_port                  = 1025
+      to_port                    = 65535
+      type                       = "egress"
+      source_node_security_group = true
+    }
+  }
+
+  # Extend node-to-node security group rules
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+    egress_all = {
+      description      = "Node all egress"
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      type             = "egress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  }
+
+  self_managed_node_group_defaults = {
+    create_security_group = false
+
+    # enable discovery of autoscaling groups by cluster-autoscaler
+    autoscaling_group_tags = {
+      "k8s.io/cluster-autoscaler/enabled" : true,
+      "k8s.io/cluster-autoscaler/${local.cluster_name}" : "owned",
+    }
+  }
+
+  self_managed_node_groups = {
+    mixed = {
+      name = "mixed"
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      ami_id               = data.aws_ami.eks_default.id
+      bootstrap_extra_args = "--kubelet-extra-args '--node-labels=node.kubernetes.io/lifecycle=spot'"
+
+      use_mixed_instances_policy = true
+      mixed_instances_policy = {
+        instances_distribution = {
+          on_demand_base_capacity                  = 0
+          on_demand_percentage_above_base_capacity = 0
+          spot_allocation_strategy                 = "capacity-optimized"
+        }
+
+        override = [
+          {
+            instance_type     = "t3.medium"
+            weighted_capacity = "2"
+          },
+          {
+            instance_type     = "t2.medium"
+            weighted_capacity = "1"
+          },
+        ]
+      }
+    }
+  }
 }
 
 data "aws_eks_cluster" "cluster" {
@@ -43,4 +112,22 @@ data "aws_eks_cluster" "cluster" {
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
+}
+
+resource "aws_kms_key" "eks" {
+  description             = "EKS Secret Encryption Key"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  tags = local.tags
+}
+
+data "aws_ami" "eks_default" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amazon-eks-node-${local.cluster_version}-v*"]
+  }
 }
